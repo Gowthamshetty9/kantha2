15b7215b0952c53a6069485e4cc7333828ae7049
DELL <DELL@DESKTOP-7114NCB>
2023-06-01 11:04:11 +0530

    hosa commit

---
+++ MyProject/src/test/java/sample1/Myp1Test.java
@@ -0,0 +1,15 @@
+package sample1;
+
+import org.testng.annotations.Test;
+
+public class Myp1Test {
+	@Test
+	public void m1() {
+		System.out.println("hi");
+	}
+
+	@Test
+	public void m4() {
+		System.out.println("hiiiiii");
+	}
+}
---
+++ MyProject/src/test/java/sample2/Ty1Test.java
@@ -0,0 +1,11 @@
+package sample2;
+
+import org.testng.annotations.Test;
+
+public class Ty1Test {
+	@Test
+public void t1()
+{
+	System.out.println("HiHello");
+}
+}
